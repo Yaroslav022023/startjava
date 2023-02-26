7ef707f930e6c398e6d14e8a67fb00244f942106
Yaroslav <set.oplata@gmail.com>
2023-02-26 22:00:21 +0100

    Done Wolf. Attempt 1

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+
+    String nickname;
+    char gender;
+    float weight;
+    int age;
+    String color;
+
+    void walk() {
+        System.out.println("Идет");
+    }
+
+    void sit() {
+        System.out.println("Сидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void voice() {
+        System.out.println("Воет");
+    }
+
+    void hunting() {
+        System.out.println("Охотится");
+    }
+}
