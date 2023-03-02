4b812b931022a3dcd77970592856861bf06ca230
Yaroslav <set.oplata@gmail.com>
2023-03-02 14:28:29 +0100

    Done Wolf. Attempt 1

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -6,43 +6,34 @@ public class Wolf {
     private int age;
     private String color;
 
-    public void walk() {
-        System.out.println("Идет");
-    }
-    public void sit() {
-        System.out.println("Сидит");
-    }
-    public void run() {
-        System.out.println("Бежит");
-    }
-    public void voice() {
-        System.out.println("Воет");
-    }
-    public void hunt() {
-        System.out.println("Охотится");
-    }
-
     public String getNickname() {
         return nickname;
     }
+
     void setNickname(String nickname) {
             this.nickname = nickname;
     }
+
     public char getGender() {
         return gender;
     }
+
     void setGender(char gender) {
         this.gender = gender;
     }
+
     public float getWeight() {
         return weight;
     }
+
     void setWeight(float weight) {
         this.weight = weight;
     }
+
     public int getAge() {
         return age;
     }
+
     void setAge(int age) {
         if (age > 8) {
             System.out.println("Incorrect age");
@@ -50,10 +41,32 @@ public class Wolf {
             this.age = age;
         }
     }
+
     public String getColor() {
         return color;
     }
+
     void setColor(String color) {
         this.color = color;
+    }
+
+    public void walk() {
+        System.out.println("Идет");
+    }
+
+    public void sit() {
+        System.out.println("Сидит");
+    }
+
+    public void run() {
+        System.out.println("Бежит");
+    }
+
+    public void howl() {
+        System.out.println("Воет");
+    }
+
+    public void hunt() {
+        System.out.println("Охотится");
     }
 }
